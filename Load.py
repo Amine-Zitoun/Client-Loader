7c672b601d11d588982df8161fc3fdceb8cafcd0
Amine Zitoun <zitoun.amino@yahoo.com>
2019-09-10 01:28:41 +0100

    first commit

---
+++ Load.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from tkinter import *
+import sqlite3 as lite
+
+
+# In[35]:
+
+
+from tkinter import ttk
+
+
+
+con = lite.connect("db.db")
+curr = con.cursor()
+root = Tk()
+root.geometry('1450x750')
+root.title('LOAD')
+table = Frame(root)
+table.pack(side="top", fill="both", expand=True)
+table.place(x=150,y=380)
+
+
+
+
+widgets={}
+def load():
+    #entry_list = [entry_2,entry_3,entry_4,entry_5]
+
+    pre = entry_2.get()
+    nd = entry_3.get()
+    doc = entry_4.get()
+    cin = entry_5.get()
+    reg = entry_6.get()
+
+    if (pre != '' and nd == '' and doc == '' and cin == '' and reg == ''):
+        #listbox.delete(1, END)
+        for i in listbox.get_children():
+            listbox.delete(i)
+        curr.execute( "SELECT * FROM DB WHERE Pernom='{}' ORDER BY Nom ".format(pre))
+
+
+        for index,row in enumerate(curr.fetchall()):
+            listbox.insert("", "end", values=(row[0], row[1], row[2],row[6],row[4],row[5],row[3]))
+            #listbox.insert(END, str(row[0])+ ' '*20 + str(row[1])+' '*20 +   str(row[2])+' '*20 +   str(row[3])+' '*20 +   str(row[4])+' '*20 +   str(row[5]))
+    if (pre == '' and nd != '' and doc == '' and cin == '' and reg == ''):
+        #listbox.delete(1, END)
+        for i in listbox.get_children():
+            listbox.delete(i)
+        curr.execute( "SELECT * FROM DB WHERE NDossier='{}' ORDER BY Nom ".format(nd))
+
+        for index,row in enumerate(curr.fetchall()):
+            listbox.insert("", "end", values=(row[0], row[1], row[2],row[6],row[4],row[5],row[3]))
+        #    print(row)
+        #    listbox.insert(END, str(row[0])+ ' '*20 + str(row[1])+' '*20 +   str(row[2])+' '*20 +   str(row[3])+' '*20 +   str(row[4])+' '*20 +   str(row[5]))
+    if (pre != '' and nd == '' and doc!= '' and cin == '' and reg == ''):
+        #listbox.delete(1, END)
+        for i in listbox.get_children():
+            listbox.delete(i)
+        curr.execute( "SELECT * FROM DB WHERE Doc='{}' ORDER BY Nom".format(doc))
+
+        for index,row in enumerate(curr.fetchall()):
+            listbox.insert("", "end", values=(row[0], row[1], row[2],row[6],row[4],row[5],row[3]))
+        #    print(row)
+        #    listbox.insert(END, str(row[0])+ ' '*20 + str(row[1])+' '*20 +   str(row[2])+' '*20 +   str(row[3])+' '*20 +   str(row[4])+' '*20 +   str(row[5]))
+    if (pre == '' and nd == '' and doc == '' and cin != '' and reg == ''):
+        for i in listbox.get_children():
+            listbox.delete(i)
+        curr.execute( "SELECT * FROM DB WHERE CIN='{}' ORDER BY Nom ".format(cin))
+
+        for index,row in enumerate(curr.fetchall()):
+            print(row)
+            listbox.insert("", "end", values=(row[0], row[1], row[2],row[6],row[4],row[5],row[3]))
+    if (pre =='' and nd == '' and doc == '' and cin == '' and reg != ''):
+        for i in listbox.get_children():
+            listbox.delete(i)
+        curr.execute( "SELECT * FROM DB WHERE REG='{}' ORDER BY Nom ".format(reg))
+
+        for index,row in enumerate(curr.fetchall()):
+            print(row)
+            listbox.insert("", "end", values=(row[0], row[1], row[2],row[6],row[4],row[5],row[3]))
+
+        #    print(row)
+        #    listbox.insert(END, str(row[0])+ ' '*20 + str(row[1])+' '*20 +   str(row[2])+' '*20 +   str(row[3])+' '*20 +   str(row[4])+' '*20 +   str(row[5]))
+    #for i in listbox.get_children():
+    #    listbox.delete(i)
+
+
+
+
+
+
+
+
+
+scrollbar = Scrollbar(root, orient="vertical")
+
+
+label_0 = Label(root,text="LOAD",width=20,font=("Arial",28,'bold'),fg='black')
+label_0.place(x=450,y=53)
+
+label2 = Label(root, text="Prenom",width=20,font=('bold',15),fg='black')
+label2.place(x=50,y=235)
+
+
+entry_2 = Entry(root,font=('bold',15))
+entry_2.place(x=230,y=235)
+
+
+label6 = Label(root, text="Region",width=20,font=('bold',15),fg='black')
+label6.place(x=270,y=160)
+
+
+entry_6 = Entry(root,font=('bold',15))
+entry_6.place(x=450,y=160)
+
+
+
+label_4 = Label(root, text="Docteur",width=20,font=('bold',15),fg='black')
+label_4.place(x=670,y=160)
+
+entry_4 = Entry(root,font=('bold',15))
+entry_4.place(x=850,y=160)
+
+
+
+
+label_5 = Label(root, text="CIN",width=20,font=('bold',15),fg='black')
+label_5.place(x=270,y=300)
+
+entry_5 = Entry(root,font=('bold',15))
+entry_5.place(x=450,y=300)
+
+
+
+label_3 = Label(root, text="N°Doss",width=20,font=('bold',15),fg='black')
+label_3.place(x=670,y=300)
+
+entry_3 = Entry(root,font=('bold',15))
+entry_3.place(x=850,y=300)
+
+
+style = ttk.Style()
+style.configure("mystyle.Treeview", highlightthickness=5, bd=0, font=('Calibri', 15),justify=LEFT) # Modify the font of the body
+style.configure("mystyle.Treeview.Heading", font=('Calibri', 20,'bold'),anchor="e") # Modify the font of the headings
+#style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])
+
+cols = ('Nom', 'Prenom', 'CIN','Region',"NDossier","Diagnostic","Docteur")
+listbox = ttk.Treeview(root, columns=cols, show='headings',style="mystyle.Treeview")
+for col in cols:
+    listbox.heading(col, text=col ,anchor=W)
+listbox.place(x=20,y=380)
+#listbox.insert(END, "Nom"+ ' '*20 + "Prenom"+' '*20 +   "N°Dossier"+' '*20 +   "Doc"+' '*20 +   "CIN"+' '*20 +   "Region" )
+
+
+
+
+Button(root,text="OK",width=10,bg='brown',fg='white',height=2 ,font=('bolder',12),command=load).place(x=580,y=680)
+
+mainloop()
+
+
+# In[ ]:
---
+++ gui.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from tkinter import *
+import sqlite3 as lite
+
+
+# In[3]:
+
+
+
+root = Tk()
+root.geometry('860x750')
+root.title('Enregister Une Malade')
+
+
+con = lite.connect("db.db")
+curr = con.cursor()
+
+def submit():
+    entry_list = [entry_1,entry_2,entry_3,entry_4,entry_5,entry_6,entry_7,entry_8]
+
+    print(entry_1.get(),
+                                                             entry_2.get(),
+                                                             entry_3.get(),
+                                                             entry_4.get(),
+         entry_5.get(),
+         entry_6.get())
+
+    curr.execute(' INSERT INTO DB(Nom,Pernom,NDossier,Doc,CIN,DG,REG) VALUES ("{}","{}","{}","{}","{}","{}","{}")'.format(entry_1.get(),
+                                                             entry_2.get(),
+                                                             entry_3.get(),
+                                                             entry_4.get(),
+                                                            entry_5.get(),
+                                                            entry_6.get(),
+                                                            entry_7.get()))
+
+    con.commit()
+    for i in entry_list:
+        i.delete(0,'end')
+
+
+mycolor = '#%02x%02x%02x' % (66,544,232)
+label_0 = Label(root,text="Enregistrement",width=20,font=("Times New Roman",28,'italic'),fg='blue')
+label_0.place(x=232,y=53)
+
+
+label_19 = Label(root,text="HOP REG GABES",width=20,font=("impact",15,'italic'),fg='black')
+label_19.place(x=18,y=53)
+label_20 = Label(root,text="SERVICE PSYCHIATRIE",width=20,font=("impact",15,'italic'),fg='black')
+label_20.place(x=18,y=80)
+
+label_1 = Label(root, text="Nom",width=20,font=('bold',15),fg='black')
+label_1.place(x=1,y=130)
+
+entry_1 = Entry(root,font=(30))
+entry_1.place(x=182,y=130)
+
+
+
+label2 = Label(root, text="PreNom",width=20,font=('bold',15),fg='black')
+label2.place(x=1,y=200)
+
+
+entry_2 = Entry(root,font=(30))
+entry_2.place(x=182,y=200)
+
+label_3 = Label(root, text="N° Doss",width=20,font=('bold',15),fg='black')
+label_3.place(x=1,y=270)
+
+entry_3 = Entry(root,font=(30))
+entry_3.place(x=182,y=270)
+
+
+label_4 = Label(root, text="Docteur",width=20,font=('bold',15),fg='red')
+label_4.place(x=380,y=130)
+
+
+entry_4 = Entry(root,font=(30))
+entry_4.place(x=580,y=130)
+
+label_5 = Label(root, text="CIN",width=20,font=('bold',15),fg='black')
+label_5.place(x=1,y=350)
+
+entry_5 = Entry(root,font=(30))
+entry_5.place(x=182,y=350)
+
+
+label_6 = Label(root, text="Diagnostic",width=20,font=('bolder',15),fg='red')
+label_6.place(x=380,y=200)
+
+entry_6 = Entry(root, width=30,font=(30))
+entry_6.place(x=580,y=200)
+
+
+
+label_7 = Label(root, text="Region",width=20,font=('bolder',15),fg='black')
+label_7.place(x=1,y=430)
+
+entry_7 = Entry(root, width=30,font=(30))
+entry_7.place(x=182,y=430)
+
+label_8 = Label(root, text="TEL",width=20,font=('bolder',15),fg='black')
+label_8.place(x=1,y=510)
+
+entry_8 = Entry(root, font=(30), width=30)
+entry_8.place(x=182,y=510)
+
+Button(root,text="OK",width=10,bg='brown',fg='white',height=2 ,font=('bolder',12),command=submit).place(x=350,y=650)
+mainloop()
+
+
+# In[6]:
+
+
+Xconn=lite.connect("db.db")
+Xc=Xconn.cursor()
+sql = "SELECT * FROM db"
+Xc.execute(sql)
+print (Xc.fetchall()) #THIS SHOWS NO RECORDS
+
+
+# In[ ]:
